6380974e30c0c00cb94b1b01f258cc6e207b10ea
Chris Tessmer <chris.tessmer@onyxpoint.com>
2016-04-15 22:46:48 -0400

    Fix tests

--- lib/simp/module/repoclosure.rb
+++ lib/simp/module/repoclosure.rb
@@ -45,21 +45,19 @@ module Simp
               tmp_dirs << mut_dir = Dir.mktmpdir('fakeforge_mut_dir_')
             end
           else
-            fail "**************** TODO: just use the dir"
+            fail "**************** TODO: just use the tarball dir"
           end
 
           download_pupmods( module_dir, mut_dir )
-          binding.pry
           package_tarballs( mut_dir, tut_dir )
 
           Dir.mktmpdir('fakeforge_pupmod_inst_dir_') do |pupmod_install_dir|
-        Dir.chdir '/tmp'
             success = run_fake_forge( tut_dir, pupmod_install_dir )
 puts "success = '#{success}'"
           end
         ensure
           tmp_dirs.each do |dir|
-        Dir.chdir '/tmp'
+            Dir.chdir '/tmp'
             FileUtils.remove_entry dir, :verbose => (@verbose > 0)
           end
         end
@@ -70,15 +68,17 @@ puts "success = '#{success}'"
         success = false
         cmd = "puppet module install #{@metadata.metadata.fetch('name')} --module_repository=http://localhost:#{@port} --modulepath=#{pupmod_install_dir}  --target-dir=#{pupmod_install_dir}"
         puts cmd
-        pidfile = File.join(mut_dir,'fakeforge.pidfile')
+        pidfile = File.join(pupmod_install_dir,'fakeforge.pidfile')
 
         Parallel.map([1,2], in_processes: 2) do |x|
           Dir.chdir '/tmp'
           if x == 1
-            puts "Starting fake forge (pidfile: '#{pidfile}'"
+            puts "Starting fake forge (module-dir: '#{tut_dir}'  pidfile: '#{pidfile}'"
             PuppetForgeServer::Server.new.go([
-              '-p', @port,
-              '-b', 'localhost', '-m', tut_dir, '--pidfile', pidfile
+              '--port', @port,
+              '--bind', 'localhost',
+              '--module-dir', tut_dir,
+              '--pidfile', pidfile
             ])
           elsif x == 2
             sleep 5
@@ -108,43 +108,19 @@ puts "success = '#{success}'"
       end
 
 
-
-
       def package_tarballs( mut_dir, tut_dir )
         pwd = Dir.pwd
-        Dir[File.join(mut_dir, 'modules', '*')].each do |f|
-          next unless File.directory? f
-          FileUtils.chdir f, :verbose => (@verbose > 0)
-          # propagate relevant environment variables
-          env_globals = []
-          [
-            'PUPPET_VERSION',
-            'STRICT_VARIABLES',
-            'FUTURE_PARSER',
-            'TRUSTED_NODE_DATA',
-            'TRAVIS',
-            'CI',
-          ].each do |v|
-            env_globals << %Q(#{v}="#{ENV[v]}") if ENV.key?( v )
-          end
-          env_globals_line = env_globals.join(' ')
-          Bundler.with_clean_env do
-            [
-             #'bundle update',
-             #'bundle exec rake build'
-             'rake build'
-            ].each do |cmd|
-              line = "#{env_globals_line} #{cmd}"
-              opts = {}
-              opts = {:out => :close, :err => :close} if @verbose == 0
-              exit 1 unless system(line, opts)
-            end
-          end
-          Dir[File.join(Dir.pwd,'pkg','*.tar.gz')].each do |tgz|
-            FileUtils.cp tgz, tut_dir, :verbose => (@verbose > 0)
-          end
-          FileUtils.chdir pwd, :verbose => (@verbose > 0)
+        Dir[File.join(mut_dir, '*')].each do |module_dir|
+          next unless File.directory? module_dir
+          FileUtils.chdir module_dir, :verbose => (@verbose > 0)
+
+          cmd = "puppet module build  --render-as=json"
+          puts cmd if @verbose > 0
+          tgz = `#{cmd}`.split("\n").last.gsub('"','')
+          puts "--[tgz] built module archive: #{tgz}" if @verbose > 0
+          FileUtils.cp tgz, tut_dir, :verbose => (@verbose > 0)
         end
+        FileUtils.chdir pwd, :verbose => (@verbose > 0)
       end
     end
   end
--- spec/lib/simp/module/repoclosure_spec.rb
+++ spec/lib/simp/module/repoclosure_spec.rb
@@ -35,25 +35,25 @@ describe Simp::Module::Repoclosure do
     end
   end
 
-###  describe '#package_tarballs' do
-###    it 'places tarballs into the destination directory' do
-###      ci = Simp::Module::Repoclosure.new( path_to_mock_module('module01') )
-###      m1 = path_to_mock_module('module01')
-###      Dir.mktmpdir('fakeforge_spec_test_mut_dir_') do |mut_dir|
-###        FileUtils.mkdir_p  File.join(mut_dir, 'modules')
-###        FileUtils.cp_r m1, File.join(mut_dir, 'modules', 'module01')
-###        Dir.mktmpdir('fakeforge_tut_dir_') do |tut_dir|
-###          ci.package_tarballs mut_dir, tut_dir
-###          expect(File).to exist( File.join( tut_dir, 'test-module01-0.1.0.tar.gz' ) )
-###        end
-###      end
-###    end
-###  end
-###
+  describe '#package_tarballs' do
+    it 'places tarballs into the destination directory' do
+
+      ci = Simp::Module::Repoclosure.new( path_to_mock_module('module01') )
+      m1 = path_to_mock_module('module01')
+      Dir.mktmpdir('fakeforge_spec_test_mut_dir_') do |mut_dir|
+        FileUtils.cp_r m1, File.join(mut_dir, 'module01')
+        Dir.mktmpdir('fakeforge_tut_dir_') do |tut_dir|
+          ci.package_tarballs mut_dir, tut_dir
+          expect(File).to exist( File.join( tut_dir, 'test-module01-0.1.0.tar.gz' ) )
+        end
+      end
+    end
+  end
+
   describe '#do' do
     it 'does a do!' do
-            Dir.chdir '/tmp'
-      ci = Simp::Module::Repoclosure.new( path_to_mock_module('module01') )
+      module_dir = path_to_mock_module('module01')
+      ci = Simp::Module::Repoclosure.new( module_dir )
       ci.verbose = 1
       ci.do
     end
